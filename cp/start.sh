10084e3fc034aa57de4ddaf7557a8fe15accf66f
hifly81 <gio.marigi@gmail.com>
2023-11-15 10:36:15 +0100

    script for linux

--- cp/start.sh
+++ cp/start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 ./stop.sh
@@ -20,7 +22,6 @@ docker exec -it broker kafka-acls --bootstrap-server broker:29092 --add --allow-
 docker exec -it broker kafka-acls --bootstrap-server broker:29092 --add --allow-principal "User:alice" --operation Read --operation Write --topic orders_v2-retry-1
 docker exec -it broker kafka-acls --bootstrap-server broker:29092 --add --allow-principal "User:alice" --operation Read --operation Write --topic orders_v2-retry-2
 docker exec -it broker kafka-acls --bootstrap-server broker:29092 --add --allow-principal "User:alice" --operation Read --operation Write --topic orders_v2-dlt
-docker exec -it broker kafka-acls --bootstrap-server broker:29092 --add --allow-principal "User:alice" --operation Read --operation Write --topic orders_ko
 
 echo "Add ACLs on consumer groups for user alice"
 docker exec -it broker kafka-acls --bootstrap-server broker:29092 --add --allow-principal "User:alice" --operation Read --group order-app-group
